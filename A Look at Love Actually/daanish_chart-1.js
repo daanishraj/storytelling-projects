4f40147d089e87fe90556080ce1245ae72b0e5ca
Daanish Raj <daanishraj@users.noreply.github.com>
2021-04-20 15:17:56 +0200

    Update daanish_chart-1.js
    Cleaning up code and comments

--- A Look at Love Actually/daanish_chart-1.js
+++ A Look at Love Actually/daanish_chart-1.js
@@ -12,10 +12,6 @@
 				.append("g")
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-	// Create your scales
-	// var colorScale = d3.scaleOrdinal().domain(["Australia", "Korea", "Estonia", 
-	// 	"Indonesia", "Colombia"]).range(["purple", "green", "orange", "red", "yellow"]);
-
 	var xPositionScale=d3.scalePoint().domain([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,
  		27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55,56,57,58,59,60,61,62,63,64,65,66,67,68,69,70,71,72,73,74,75,77,78,79]).range([0, width])
 	.padding(2)
@@ -64,19 +60,12 @@
 			return yPositionScale(d.num_character)
 		})
 
-		// You have many, many datapoints but only need a few lines...
-		// how do you group them together?
-
-
 	svg.append("path")
     .datum(datapoints)
     .attr("d", line)
     .attr("stroke", "green")
     .attr("fill", "none")
     .attr("stroke-width", 2)
-
-
-
 
 		// Add your axes
 
@@ -92,12 +81,6 @@
       .call(yAxis);
 
 	}
-
-
-// how do I label my axes?
-// too many values very close to each other on x axis
-
-
 
 
 })();
